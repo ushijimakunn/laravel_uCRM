29f8d04ac6fb700a0429de0094edb5be86754296
ushijima eisaku <eisakuushijima@gmail.com>
2023-11-23 14:26:20 +0900

    Section2

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'
+
+</script>
+
+<template>
+Inertiaテストです。<br>
+<a href="/">aタグ経由です</a><br>
+<Link href="/">Link経由です。</Link><br>
+<Link :href="route('inertia.index')">Index.vueへのリンクです。</Link><br>
+<Link :href="route('inertia.show', { id: 50 })">ルートパラメータのテストです。</Link>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\InertiaTestController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +17,15 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/inertia-test', function () {
+    return Inertia::render('InertiaTest');
+    }
+);
+
+Route::get('/inertia/index', [InertiaTestController::class, 'index'])->name('inertia.index');
+Route::get('/inertia/show/{id}', [InertiaTestController::class, 'show'])->name('inertia.show');
+
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
