7c2abb63b7fd7efeb568c2db1a545409fd27790c
ushijima eisaku <eisakuushijima@gmail.com>
2023-11-23 17:55:44 +0900

    add index page

--- resources/js/Pages/Inertia/Index.vue
+++ resources/js/Pages/Inertia/Index.vue
@@ -1,4 +1,8 @@
 <script setup>
+    import { Link } from '@inertiajs/vue3';
+    defineProps({
+        blogs: Array
+    })
 
 </script>
 <template>
@@ -6,4 +10,11 @@
         {{$page.props.flash.message}}
     </div>
     <h1>Index Page</h1>
+    <ul>
+        <li v-for="blog in blogs" :key="blog.id">
+                件名：<Link class="text-blue-400" :href="route('inertia.show', { id: blog.id })"> {{ blog.title }} </Link>
+                本文：<Link> {{ blog.content }} </Link>
+        </li>
+    </ul>
+
 </template>
