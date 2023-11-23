d5c999e72995c89da583eefa18ec1e17e44d6809
ushijima eisaku <eisakuushijima@gmail.com>
2023-11-23 16:26:36 +0900

    add server side validation

--- resources/js/Pages/Inertia/Create.vue
+++ resources/js/Pages/Inertia/Create.vue
@@ -2,6 +2,10 @@
     import { reactive } from 'vue';
     import { Inertia } from '@inertiajs/inertia';
 
+    defineProps({
+        errors: Object
+    })
+
     const form = reactive({
         title: null,
         content: null
@@ -14,8 +18,10 @@
 
 <template>
     <form @submit.prevent="submitFunction">
-        <input type="text" name="title" v-model="form.title">
-        <input type="text" name="content" v-model="form.content">
+        <input type="text" name="title" v-model="form.title"><br>
+        <div v-if="errors.title">{{ errors.title }}</div>
+        <input type="text" name="content" v-model="form.content"><br>
+        <div v-if="errors.content">{{ errors.content }}</div>
         <button>送信</button>
     </form>
 </template>
