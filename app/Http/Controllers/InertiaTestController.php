fad4eada141a805a8f270fe7297a01b4808477d1
ushijima eisaku <eisakuushijima@gmail.com>
2023-11-24 12:13:58 +0900

    add delete function

--- app/Http/Controllers/InertiaTestController.php
+++ app/Http/Controllers/InertiaTestController.php
@@ -25,7 +25,8 @@ class InertiaTestController extends Controller
         // dd($id);
         return Inertia::render('Inertia/Show',
         [
-            'id' => $id
+            'id' => $id,
+            'blog' => InertiaTest::findOrFail($id)
         ]);
     }
 
@@ -47,5 +48,16 @@ class InertiaTestController extends Controller
         ]);
 
     }
+
+     public function delete($id)
+     {
+        $book = InertiaTest::findOrFail($id);
+        $book->delete();
+
+        return to_route('inertia.index')
+        ->with([
+            'message' => '削除しました。'
+        ]);
+     }
 
 }
