4d4bcd37eafd4426c70fc9da5228b5f1e001cc74
ushijima eisaku <eisakuushijima@gmail.com>
2023-11-24 12:50:32 +0900

    add component test

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,11 @@ Route::get('/inertia-test', function () {
     }
 );
 
+Route::get('/component-test', function () {
+    return Inertia::render('ComponentTest');
+    }
+);
+
 Route::get('/inertia/index', [InertiaTestController::class, 'index'])->name('inertia.index');
 Route::get('/inertia/create', [InertiaTestController::class, 'create'])->name('inertia.create');
 Route::post('/inertia', [InertiaTestController::class, 'store'])->name('inertia.store');
