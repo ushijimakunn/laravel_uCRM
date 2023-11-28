485d695cec5977268a5ac3db07a9c5350e1cf274
ushijima eisaku <eisakuushijima@gmail.com>
2023-11-28 21:32:43 +0900

    add items index page

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -17,7 +17,11 @@ class ItemController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Items/Index');
+        $items = Item::select('id', 'name', 'price', 'is_selling')->get();
+
+        return Inertia::render('Items/Index', [
+            'items' => $items
+        ]);
     }
 
     /**
@@ -27,7 +31,7 @@ class ItemController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Items/Create');
     }
 
     /**
--- resources/js/Pages/Items/Create.vue
+++ resources/js/Pages/Items/Create.vue
@@ -1,14 +1,14 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <Head title="Dashboard" />
+    <Head title="商品登録" />
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">商品登録</h2>
         </template>
 
         <div class="py-12">
