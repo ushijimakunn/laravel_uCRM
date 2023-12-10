3b91474982a937697708523864862c99c5b21e62
ushijima eisaku <eisakuushijima@gmail.com>
2023-12-10 18:35:18 +0900

    add postNum search

--- resources/js/Pages/Customers/Create.vue
+++ resources/js/Pages/Customers/Create.vue
@@ -3,7 +3,8 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, Link } from '@inertiajs/vue3';
 import { reactive } from 'vue';
 import { Inertia } from '@inertiajs/inertia';
-import BreezeValidationErrors from '@/Components/ValidationErrors.vue'
+import BreezeValidationErrors from '@/Components/ValidationErrors.vue';
+import {Core as YubinBangoCore} from 'yubinbango-core2';
 
 defineProps({
     errors: Object
@@ -20,6 +21,13 @@ const form = reactive({
     gender: null,
     memo: null
 })
+
+const feachAddress = () => {
+    new YubinBangoCore(String(form.postcode), (value) => {
+        // console.log(value)
+        form.address = value.region + value.locality + value.street
+    })
+}
 
 const storeCustomer = () => {
     Inertia.post('/customers', form)
@@ -71,7 +79,7 @@ const storeCustomer = () => {
                                             <div class="p-2 w-full">
                                                 <div class="relative">
                                                     <label for="postcode" class="leading-7 text-sm text-gray-600">郵便番号</label>
-                                                    <input type="text" id="postcode" name="postcode" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                    <input type="text" id="postcode" name="postcode" @change="feachAddress" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                                 </div>
                                             </div>
                                             <div class="p-2 w-full">
