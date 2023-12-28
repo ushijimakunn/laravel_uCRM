d626c0821f4ca47eb557bea7b01dca4760c7e3af
ushijima eisaku <eisakuushijima@gmail.com>
2023-12-28 16:07:55 +0900

    add item information

--- resources/js/Pages/Purchases/Create.vue
+++ resources/js/Pages/Purchases/Create.vue
@@ -1,13 +1,41 @@
 <script setup>
 import { getToday } from '@/common';
-import { onMounted, reactive } from 'vue';
+import { onMounted, reactive, ref, computed } from 'vue';
+
+const props = defineProps({
+    'customers' : Array,
+    'items' : Array,
+});
 
 // ページ読み込み後に即実行
 onMounted(() => {
     form.date = getToday();
+    props.items.forEach((item) => {
+        itemList.value.push({
+            id: item.id,
+            name: item.name,
+            price: item.price,
+            quantity: 0,
+        })
+    });
 })
 
-const form = reactive({ date: null });
+const itemList = ref([]);
+
+const form = reactive({
+    date: null,
+    customer_id: null,
+ });
+
+const totalPrice = computed(() => {
+    let total = 0
+    itemList.value.forEach((item) => {
+        total += item.price * item.quantity
+    })
+    return total
+});
+
+ const quantity = ["0", "1", "2", "3","4", "5","6", "7","8", "9"];
 
 </script>
 
@@ -15,5 +43,47 @@ const form = reactive({ date: null });
 <h1>purchases/create</h1>
 日付<br>
 <input type="date" name="date" v-model="form.date">
+<br>
+<select name="customer" v-model="form.customer_id">
+    <option v-for="customer in customers" :value="customer.id" :key="customer.id">
+        {{ customer.id }} : {{ customer.name }}
+    </option>
+</select>
+<br>
+<br>
+
+商品・サービス<br>
+<table>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>商品名</th>
+            <th>金額</th>
+            <th>数量</th>
+            <th>小計</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="item in itemList" :key="item.id">
+            <td>{{ item.id }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.price }}</td>
+            <td>
+                <select name="quantity" v-model="item.quantity">
+                    <option v-for="q in quantity" :value="q" :key="q">
+                        {{ q }}
+                    </option>
+                </select>
+            </td>
+            <td>
+                {{ item.price * item.quantity }}
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+<br>
+合計 : {{ totalPrice }} 円
+
 
 </template>
