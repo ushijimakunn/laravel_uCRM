3098c4d314b865af2302955ac9863de0af8220a0
ushijima eisaku <eisakuushijima@gmail.com>
2023-12-29 15:19:21 +0900

    add modal to Purchase/Create

--- resources/js/Pages/Purchases/Create.vue
+++ resources/js/Pages/Purchases/Create.vue
@@ -5,7 +5,7 @@ import { onMounted, reactive, ref, computed } from 'vue';
 import { Inertia } from '@inertiajs/inertia';
 import BreezeValidationErrors from '@/Components/ValidationErrors.vue'
 import { getToday } from '@/common';
-
+import MicroModal from '@/Components/MicroModal.vue'
 
 const props = defineProps({
     'customers' : Array,
@@ -86,6 +86,7 @@ const storePurchase = () => {
                                             </div>
                                             <div class="p-2 w-full">
                                                 <div class="relative">
+                                                    <MicroModal />
                                                     <label for="customer" class="leading-7 text-sm text-gray-600">会員名</label>
                                                     <select id="customer" name="customer" v-model="form.customer_id" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                                         <option v-for="customer in customers" :value="customer.id" :key="customer.id">
